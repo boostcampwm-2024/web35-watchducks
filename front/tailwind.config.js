ee4dc845b5ca1cacd25ff0da3646f74acd82b707
Hosung99 <shson1217@kw.ac.kr>
2024-11-06 16:31:54 +0900

    setting: tailwind config초기화

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,11 +2,7 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {
-      fontSize: {
-        xs: ['12px', { lineHeight: '18px' }]
-      }
-    }
+    extend: {}
   },
   plugins: []
 };
