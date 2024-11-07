ed3c024a3c98460fa018e8192f36bb610d236982
sjy2335 <joey110951@gmail.com>
2024-11-08 03:01:11 +0900

    chore: 도메인 충돌 에러코드 확인 함수 분리

--- backend/console-server/src/project/project.service.ts
+++ backend/console-server/src/project/project.service.ts
@@ -1,7 +1,7 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Project } from './entities/project.entity';
-import { Repository } from 'typeorm';
+import { QueryFailedError, Repository } from 'typeorm';
 
 @Injectable()
 export class ProjectService {
@@ -15,11 +15,20 @@ export class ProjectService {
       const result = await this.projectRepository.insert(projectData);
       return result.identifiers;
     } catch (error) {
-      if (error.code === 'ER_DUP_ENTRY')
+      if (
+        error instanceof QueryFailedError &&
+        isUniqueConstraintViolation(error.driverError)
+      ) {
         throw new ConflictException(
-          'Project with the same domain or ID already exists.',
+          'Project with the same domain already exists.',
         );
+      }
       throw error;
     }
   }
+}
+
+function isUniqueConstraintViolation(error: any): boolean {
+  const uniqueViolationCodes = ['ER_DUP_ENTRY', '23505'];
+  return uniqueViolationCodes.includes(error.code);
 }
