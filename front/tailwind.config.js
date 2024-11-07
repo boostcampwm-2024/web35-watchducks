7329f0c54deb47049c18317131cb6e7066e500aa
Hosung99 <shson1217@kw.ac.kr>
2024-11-08 00:50:35 +0900

    setting: tailwind 색상 설정 추가

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,7 +2,35 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        blue: {
+          DEFAULT: '#0079FF',
+          light: '#0079FF',
+          dark: '#0079FF'
+        },
+        gray: {
+          DEFAULT: '#91A3B7',
+          light: '#91A3B7',
+          dark: '#91A3B7'
+        },
+        green: {
+          DEFAULT: '#00DD4B',
+          light: '#00DD4B',
+          dark: '#00DD4B'
+        },
+        red: {
+          DEFAULT: '#FF7676',
+          light: '#FF7676',
+          dark: '#FF7676'
+        },
+        black: {
+          DEFAULT: '#001940',
+          light: '#001940',
+          dark: '#001940'
+        }
+      }
+    }
   },
   plugins: []
 };
