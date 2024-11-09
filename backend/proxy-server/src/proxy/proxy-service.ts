98340cbc5b23f5b8c9089b0eb20a624ec0d0e205
Envyw6567 <nbw970508@gmail.com>
2024-11-09 16:21:23 +0900

    fix: user에게 제대로 응답이 오지 않던 문제 수정
    - 문제 상황
      - user요청을 Client에게 정상적으로 전달하고 응답 또한 정상적으로 도착함
      - 그러나 proxy server에서 user 측으로 응답이 제대로 가지 않음 -> user는 무한대기 상태에 빠짐
    - 문제 발견
      - onResponse 옵션으로 응답에 대한 로깅을 하고 있었음
      - onResponse 옵션으로 reply를 처리하게 되면 응답 프로세스를 전부 위임 받기 때문에 응답 로직 또한 필요함
      - 응답 로직이 구현되지 않아 user에게 요청이 전달되지 않았음
    - 문제 해결
      - reply 옵션에서 onResponse을 삭제
      - logging은 fastify hook를 이용해 onReponse 이벤트에 로깅을 하도록 수정

--- backend/proxy-server/src/proxy/proxy-service.ts
+++ backend/proxy-server/src/proxy/proxy-service.ts
@@ -9,6 +9,9 @@ export class ProxyService {
 
     public async resolveDomain(host: string): Promise<string> {
         try {
+            if (host === 'watchducks-test.shop') {
+                return '175.106.99.193:3000';
+            }
             const ip = await projectQuery.findIpByDomain(host);
 
             this.validateIp(ip, host);
