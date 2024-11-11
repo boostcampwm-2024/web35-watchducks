f00ff1e603347229ac3f8c800e295c67dd5bf3b4
sjy2335 <joey110951@gmail.com>
2024-11-11 22:25:49 +0900

    deploy: CI/CD 테스트 - deploy-service.yml에 공통 작업 - ci-cd.yml에 하위 레포지토리 작업

--- backend/console-server/src/main.ts
+++ backend/console-server/src/main.ts
@@ -6,6 +6,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  const server = await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+
+  process.on('SIGTERM', () => {
+    console.log('SIGTERM signal received: closing HTTP server');
+    server.close(() => {
+      console.log('HTTP server closed');
+      process.exit(0);
+    });
+  });
 }
 bootstrap();
