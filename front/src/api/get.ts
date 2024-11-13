5f54586e36830becd73ba0957c4564da11194804
Hosung99 <shson1217@kw.ac.kr>
2024-11-13 18:37:15 +0900

    feat: 해당 기수의 그룹이름들을 가져오는 api추가

---
+++ front/src/api/get.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const getGroups = async (generation: string) => {
+  const response = await axios.get(
+    `${import.meta.env.VITE_API_URL}/api/groups?generation=${generation}`
+  );
+  return response.data;
+};
+
+export { getGroups };
