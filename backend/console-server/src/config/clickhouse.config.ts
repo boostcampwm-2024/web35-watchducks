2666b381ae4945efac755196d0f0d77087d93869
Envyw6567 <nbw970508@gmail.com>
2024-11-13 13:49:38 +0900

    setting: clickhouse config 수정
    - 키값 변경 : host -> url

--- backend/console-server/src/config/clickhouse.config.ts
+++ backend/console-server/src/config/clickhouse.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('clickhouse', () => ({
     clickhouse: {
-        host: process.env.CLICKHOUSE_URL ?? 'http://localhost:8123',
+        url: process.env.CLICKHOUSE_URL ?? 'http://localhost:8123',
         username: process.env.CLICKHOUSE_USER ?? 'default',
         database: process.env.CLICKHOUSE_DATABASE ?? 'default',
         password: process.env.CLICKHOUSE_PASSWORD ?? '',
