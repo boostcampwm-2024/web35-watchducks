4ed4bdce321e59523ebce6d871d877bf99e4083c
Hosung99 <shson1217@kw.ac.kr>
2024-11-13 17:14:30 +0900

    feat: Navbar레이아웃 적용

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,19 @@
 import MainPage from '@component/page/MainPage';
+import ProjectPage from '@component/page/ProjectPage';
+import RankingPage from '@component/page/RankingPage';
 import RegisterPage from '@component/page/RegisterPage';
+import MainPageLayout from '@component/template/MainPageLayout';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<MainPage />}></Route>
-      <Route path='/register' element={<RegisterPage />}></Route>
+      <Route element={<MainPageLayout />}>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/project' element={<ProjectPage />} />
+        <Route path='/ranking' element={<RankingPage />} />
+      </Route>
+      <Route path='/register' element={<RegisterPage />} />
     </Routes>
   );
 }
