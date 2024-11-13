f68b52ea374211ccc7597ef441569988c0a343ec
Hosung99 <shson1217@kw.ac.kr>
2024-11-13 18:14:23 +0900

    refactor: QueryReserErrorboundary로 수정

--- front/src/boundary/CustomErrorBoundary.tsx
+++ front/src/boundary/CustomErrorBoundary.tsx
@@ -1,14 +1,28 @@
+import Loading from '@component/atom/Loading';
+import { QueryErrorResetBoundary } from '@tanstack/react-query';
 import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 
 import CustomErrorFallback from './CustomErrorFallback';
 
-import Loading from '@/component/atom/Loading';
-
 export default function CustomErrorBoundary({ children }: { children: React.ReactElement }) {
+  console.log(children);
   return (
-    <ErrorBoundary FallbackComponent={CustomErrorFallback}>
-      <Suspense fallback={<Loading />}>{children}</Suspense>
-    </ErrorBoundary>
+    <QueryErrorResetBoundary>
+      {({ reset }) => (
+        <ErrorBoundary onReset={reset} FallbackComponent={CustomErrorFallback}>
+          <div className='w-full'>
+            <Suspense
+              fallback={
+                <div className='flex h-full w-full items-center justify-center'>
+                  <Loading />
+                </div>
+              }>
+              {children}
+            </Suspense>
+          </div>
+        </ErrorBoundary>
+      )}
+    </QueryErrorResetBoundary>
   );
 }
