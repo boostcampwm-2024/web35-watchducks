e1aaee98acb81eb6acb78b5598716a529534a567
Hosung99 <shson1217@kw.ac.kr>
2024-11-13 17:18:48 +0900

    refactor: 이미지 alt 설정 추가 및 css 수정

--- front/src/component/molecule/RegisterText.tsx
+++ front/src/component/molecule/RegisterText.tsx
@@ -6,15 +6,19 @@ import P from '@component/atom/P';
 
 export default function RegisterText() {
   return (
-    <div className='text-18 flex flex-col gap-10'>
-      <H1 cssOption='font-medium text-50' content='등록 절차' />
+    <div className='flex flex-col gap-10 text-18'>
+      <H1 cssOption='font-medium text-50 border-2' content='등록 절차' />
       <P content='1. 프로젝트명, 도메인, 아이피 주소를 입력합니다.' />
       <P content='2. 등록 완료 후 표시된 네임 서버를 복사합니다.' />
       <P content='3. 상용 DNS (ex. Cloudflare, Gabia)의 가이드에 따라 네임 서버와 IP를 등록해 주세요.' />
       <P cssOption='mt-20 font-light' content='Gabia 예시' />
-      <Img cssOption='bg-cover' src={GabiaDescriptionImg} />
+      <Img cssOption='bg-cover' src={GabiaDescriptionImg} alt='가비아 dns 등록 설명 이미지' />
       <P cssOption='mt-20 font-light' content='CloudFlare 예시' />
-      <Img cssOption='bg-cover' src={CloudFlareDescriptionImg} />
+      <Img
+        cssOption='bg-cover'
+        src={CloudFlareDescriptionImg}
+        alt='클라우드플레어 dns 등록 설명 이미지'
+      />
     </div>
   );
 }
