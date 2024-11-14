8d5f1c95354050d9e5e5e820db6389df1431c1e7
sjy2335 <joey110951@gmail.com>
2024-11-14 16:21:12 +0900

    fix: 프록시 서버 https 넘기기

--- backend/proxy-server/src/server/proxy-server.ts
+++ backend/proxy-server/src/server/proxy-server.ts
@@ -39,6 +39,9 @@ export class ProxyServer {
                 connections: Number(process.env.DEFAULT_CONNECTIONS),
                 pipelining: Number(process.env.DEFAULT_PIPELINING),
                 keepAliveTimeout: Number(process.env.DEFAULT_KEEP_ALIVE),
+                connect: {
+                    rejectUnauthorized: false,
+                },
             },
         });
     }
@@ -89,7 +92,7 @@ export class ProxyServer {
     private async executeProxyRequest(request: FastifyRequest, reply: FastifyReply): Promise<void> {
         const host = validateHost(request.headers[HOST_HEADER]);
         const ip = await this.resolveDomain(host);
-        const targetUrl = buildTargetUrl(ip, request.url, 'http://'); // TODO: Protocol 별 arg 세팅
+        const targetUrl = buildTargetUrl(ip, request.url, 'https://'); // TODO: Protocol 별 arg 세팅
 
         await this.sendProxyRequest(targetUrl, request, reply);
     }
@@ -114,6 +117,8 @@ export class ProxyServer {
         request: FastifyRequest,
         reply: FastifyReply,
     ): Promise<void> {
+        const originalHost = request.headers[HOST_HEADER] as string;
+
         await new Promise<void>((resolve, reject) => {
             reply.from(targetUrl, {
                 onError: (reply, error) => {
@@ -125,6 +130,11 @@ export class ProxyServer {
                         ),
                     );
                 },
+
+                rewriteRequestHeaders: (req, headers) => ({
+                    ...headers,
+                    host: originalHost,
+                }),
             });
         });
     }
