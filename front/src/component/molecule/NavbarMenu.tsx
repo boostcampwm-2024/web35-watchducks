4e27ee87ff031fb8e1da120566b81fd4786f575a
Hosung99 <shson1217@kw.ac.kr>
2024-11-19 19:07:29 +0900

    refactor: 분리된 컴포넌트 병합

--- front/src/component/molecule/NavbarMenu.tsx
+++ front/src/component/molecule/NavbarMenu.tsx
@@ -1,16 +1,36 @@
 import H1 from '@component/atom/H1';
-import NavbarMenuItem from '@component/molecule/NavbarMenuItem';
+import Img from '@component/atom/Img';
+import P from '@component/atom/P';
 import { MENU_ITEMS } from '@constant/NavbarMenu';
-import { useLocation } from 'react-router-dom';
+import { MenuItem } from '@type/Navbar';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function NavbarMenu() {
   const { pathname } = useLocation();
+
+  function MenuItem({ item, isActive }: { item: MenuItem; isActive: boolean }) {
+    return (
+      <Link to={item.path}>
+        <div
+          className={`flex cursor-pointer items-center gap-4 md:gap-8 ${
+            isActive ? 'text-black' : 'text-gray hover:text-black'
+          }`}>
+          <Img
+            src={isActive ? item.activeIcon : item.inactiveIcon}
+            alt='메뉴제목 텍스트'
+            cssOption='w-4 md:w-6'
+          />
+          <P content={item.label} cssOption='text-12 md:text-14 whitespace-nowrap' />
+        </div>
+      </Link>
+    );
+  }
 
   return (
     <div className='flex flex-col gap-8 md:gap-16'>
       <H1 cssOption='mt-8 text-14 md:text-16 whitespace-nowrap' content='MENU' />
       {MENU_ITEMS.map((item) => (
-        <NavbarMenuItem key={item.path} item={item} isActive={pathname === item.path} />
+        <MenuItem key={item.path} item={item} isActive={pathname === item.path} />
       ))}
     </div>
   );
