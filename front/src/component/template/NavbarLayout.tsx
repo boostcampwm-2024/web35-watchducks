31b173e8e4fb6025a73ded72dde88d4aa859a77b
Hosung99 <shson1217@kw.ac.kr>
2024-11-19 21:42:19 +0900

    feat: 페이지 래핑 바운더리 추가

--- front/src/component/template/NavbarLayout.tsx
+++
@@ -1,9 +0,0 @@
-import Navbar from '@component/organism/NavBar';
-
-export default function NavbarLayout() {
-  return (
-    <aside className='h-screen bg-white'>
-      <Navbar />
-    </aside>
-  );
-}
