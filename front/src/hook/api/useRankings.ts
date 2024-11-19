3d93ee1a8486e70693876c1fe90322c9b5af18c7
Hosung99 <shson1217@kw.ac.kr>
2024-11-19 21:40:43 +0900

    style: 오타 수정

--- front/src/hook/api/useRankings.ts
+++ front/src/hook/api/useRankings.ts
@@ -1,6 +1,6 @@
 import { getRakings } from '@api/get';
 import { useSuspenseQuery } from '@tanstack/react-query';
-import { Ranking } from '@type/Navbar';
+import { Ranking } from '@type/api';
 
 export default function useRankings() {
   return useSuspenseQuery<Ranking[]>({
