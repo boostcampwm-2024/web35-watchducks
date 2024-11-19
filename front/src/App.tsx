1ab7ce3ea5970784fd2908f765cdee2f8e51e8c5
Hosung99 <shson1217@kw.ac.kr>
2024-11-19 19:05:58 +0900

    style: 변수명 수정

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,13 +2,14 @@ import MainPage from '@component/page/MainPage';
 import ProjectPage from '@component/page/ProjectPage';
 import RankingPage from '@component/page/RankingPage';
 import RegisterPage from '@component/page/RegisterPage';
-import MainPageLayout from '@component/template/MainPageLayout';
 import { Routes, Route } from 'react-router-dom';
+
+import MainBoundary from './boundary/MainBoundary';
 
 function App() {
   return (
     <Routes>
-      <Route element={<MainPageLayout />}>
+      <Route element={<MainBoundary />}>
         <Route path='/' element={<MainPage />} />
         <Route path='/project' element={<ProjectPage />} />
         <Route path='/ranking' element={<RankingPage />} />
