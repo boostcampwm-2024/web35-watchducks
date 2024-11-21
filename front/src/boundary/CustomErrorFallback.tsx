6c66c2468720a6e2593108728558c82aeb624911
Hosung99 <shson1217@kw.ac.kr>
2024-11-21 16:00:29 +0900

    refactor: error시 loading컴포넌트 중앙 정렬

--- front/src/boundary/CustomErrorFallback.tsx
+++ front/src/boundary/CustomErrorFallback.tsx
@@ -14,11 +14,11 @@ export default function CustomErrorFallback({ resetErrorBoundary }: Props) {
   };
 
   return (
-    <div className='flex flex-col items-center gap-4'>
+    <div className='flex flex-col items-center justify-center gap-4'>
       <Loading />
       <button
         onClick={handleClickReset}
-        className='flex items-center gap-2 rounded-lg bg-blue px-6 py-2 text-white shadow-md transition-all duration-200 hover:bg-opacity-90 active:scale-95'>
+        className='flex items-center gap-2 rounded-lg bg-blue px-6 py-2 text-white shadow-md transition-all duration-200 hover:bg-opacity-90 hover:text-black active:scale-95'>
         재시도
       </button>
     </div>
