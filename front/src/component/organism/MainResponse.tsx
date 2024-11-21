03931ff0ed7e01785a832aea9abf678ec5a04c0a
Hosung99 <shson1217@kw.ac.kr>
2024-11-21 16:17:14 +0900

    feat: TOP5 ResponseTime 컴포넌트 추가

---
+++ front/src/component/organism/MainResponse.tsx
@@ -0,0 +1,68 @@
+import { BarChart } from '@chart/BarChart';
+import DataLayout from '@component/template/DataLayout';
+import useTop5ResponseTime from '@hook/api/useTop5ResponseTime';
+
+type Props = {
+  generation: string;
+};
+
+export default function MainResponse({ generation }: Props) {
+  const { data } = useTop5ResponseTime(generation);
+
+  const series = [
+    {
+      data: data.map((item) => item.avgResponseTime)
+    }
+  ];
+
+  const categories = data.map((item) => item.projectName).map((projectName) => [projectName]);
+
+  const colors = ['#FF6B6B', '#FF9F43', '#FECA57', '#4ECB71', '#4B7BEC'];
+
+  const options: ApexCharts.ApexOptions = {
+    chart: {
+      height: 350,
+      toolbar: {
+        show: false
+      }
+    },
+    xaxis: {
+      categories,
+      labels: {
+        style: {
+          colors: '#64748B',
+          fontSize: '12px'
+        }
+      }
+    },
+    colors: colors,
+    tooltip: {
+      custom: function ({ series, seriesIndex, dataPointIndex }) {
+        const color = colors[dataPointIndex];
+        return (
+          '<div class="custom-tooltip" style="padding: 8px;">' +
+          '<span style="color: ' +
+          color +
+          ';">●</span> ' +
+          '<span style="color: #000000;">' +
+          data[dataPointIndex].projectName +
+          ': ' +
+          series[seriesIndex][dataPointIndex] +
+          'ms</span>' +
+          '</div>'
+        );
+      }
+    }
+  };
+
+  return (
+    <DataLayout cssOption='flex flex-col p-8 rounded-lg shadow-md w-full'>
+      <div className='mb-8 text-center'>
+        <h2 className='text-navy text-2xl font-bold'>TOP5 Response Speed</h2>
+      </div>
+      <div className='w-full flex-1'>
+        <BarChart options={options} series={series} />
+      </div>
+    </DataLayout>
+  );
+}
