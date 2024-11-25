f30c285bfbbc7d2dd4b18e1578be81aa58d56917
hyo-limilimee <1001lily@naver.com>
2024-11-25 20:30:48 +0900

    fix: local time에 맞게 로그 저장 시간 변경
    - UTC + 9 반영하도록 변경

--- backend/proxy-server/src/database/query/log.repository.clickhouse.ts
+++ backend/proxy-server/src/database/query/log.repository.clickhouse.ts
@@ -36,12 +36,14 @@ export class LogRepositoryClickhouse implements LogRepository {
     }
 
     private formatDate(date: Date): string {
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const day = String(date.getDate()).padStart(2, '0');
-        const hours = String(date.getHours()).padStart(2, '0');
-        const minutes = String(date.getMinutes()).padStart(2, '0');
-        const seconds = String(date.getSeconds()).padStart(2, '0');
+        const localTime = new Date(date.getTime() + 9 * 60 * 60 * 1000);
+
+        const year = localTime.getFullYear();
+        const month = String(localTime.getMonth() + 1).padStart(2, '0');
+        const day = String(localTime.getDate()).padStart(2, '0');
+        const hours = String(localTime.getHours()).padStart(2, '0');
+        const minutes = String(localTime.getMinutes()).padStart(2, '0');
+        const seconds = String(localTime.getSeconds()).padStart(2, '0');
 
         return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
     }
