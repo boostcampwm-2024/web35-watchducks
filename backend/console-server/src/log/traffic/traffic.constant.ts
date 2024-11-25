60346f4b021fccc89d6e68d3eeadbb0c2d3e179b
sjy2335 <joey110951@gmail.com>
2024-11-26 00:52:06 +0900

    chore: 타임유닛 상수 클릭하우스 지형식으로 수정

--- backend/console-server/src/log/traffic/traffic.constant.ts
+++ backend/console-server/src/log/traffic/traffic.constant.ts
@@ -4,16 +4,16 @@ export const TIME_RANGE = {
     MONTH: '1month',
 } as const;
 
-export const TIME_UNIT = {
-    ONE_MINUTE: '1 minute',
-    TWENTY_MINUTES: '20 minute',
-    ONE_HOUR: '1 hour',
+export const CLICKHOUSE_TIME_UNIT = {
+    ONE_MINUTE: 'Minute',
+    FIFTEEN_MINUTES: 'FifteenMinutes',
+    ONE_HOUR: 'Hour',
 };
 
 export const TIME_RANGE_UNIT_MAP = {
-    [TIME_RANGE.DAY]: TIME_UNIT.ONE_MINUTE,
-    [TIME_RANGE.WEEK]: TIME_UNIT.TWENTY_MINUTES,
-    [TIME_RANGE.MONTH]: TIME_UNIT.ONE_HOUR,
+    [TIME_RANGE.DAY]: CLICKHOUSE_TIME_UNIT.ONE_MINUTE,
+    [TIME_RANGE.WEEK]: CLICKHOUSE_TIME_UNIT.FIFTEEN_MINUTES,
+    [TIME_RANGE.MONTH]: CLICKHOUSE_TIME_UNIT.ONE_HOUR,
 } as const;
 
 export type TimeRange = keyof typeof TIME_RANGE_UNIT_MAP;
