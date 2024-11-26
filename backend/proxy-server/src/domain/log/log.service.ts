3ec9cf2d90486f8d97d41caf3a695618630c757f
hyo-limilimee <1001lily@naver.com>
2024-11-26 14:54:23 +0900

    fix: try-catch문 삭제
    - 바깥쪽에서 throw 해주고 있어 try-catch문 삭제

--- backend/proxy-server/src/domain/log/log.service.ts
+++ backend/proxy-server/src/domain/log/log.service.ts
@@ -4,10 +4,6 @@ export class LogService {
     constructor(private readonly logRepository: LogRepositoryClickhouse) {}
 
     public async saveHttpLog(log: HttpLogEntity): Promise<void> {
-        try {
             await this.logRepository.insertHttpLog(log);
-        } catch (error) {
-            throw error;
-        }
     }
 }
