1ca91ba84cf951b069e3a217c16be666664c495d
sjy2335 <joey110951@gmail.com>
2024-11-26 21:51:07 +0900

    chore: 응답 dto에 개수까지 포함

--- backend/console-server/src/log/rank/dto/get-elapsed-time-rank-response.dto.ts
+++ backend/console-server/src/log/rank/dto/get-elapsed-time-rank-response.dto.ts
@@ -1,17 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Expose, Type } from 'class-transformer';
+import { Expose } from 'class-transformer';
 import { IsNumber, IsString } from 'class-validator';
 
 export class ElapsedTimeRank {
     @IsString()
+    @Expose()
     projectName: string;
 
-    @Type(() => Number)
     @IsNumber()
+    @Expose()
     elapsedTime: number;
 }
 
 export class GetElapsedTimeRankResponseDto {
+    @ApiProperty({
+        description: '총 갯수',
+        example: 30,
+    })
+    @IsNumber()
+    total: number;
+
     @ApiProperty({
         description: '응답 소요 시간 짧은 순으로 정렬된 프로젝트명과 시간(ms)',
         example: [
