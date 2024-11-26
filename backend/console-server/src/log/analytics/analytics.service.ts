5de6c97097d4f33fa5da023d8f3b324f6aa3b98f
sjy2335 <joey110951@gmail.com>
2024-11-26 16:11:05 +0900

    feat: 요구사항 수정에 맞게 dau 서비스 레이어 수정 - 비어있는 레코드 값 0으로 채우기

--- backend/console-server/src/log/analytics/analytics.service.ts
+++ backend/console-server/src/log/analytics/analytics.service.ts
@@ -1,11 +1,12 @@
 import { AnalyticsRepository } from './analytics.repository';
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { GetProjectDAU } from './dto/get-project-dau.dto';
+import { GetDAUsByProjectDto } from './dto/get-project-dau.dto';
 import { plainToInstance } from 'class-transformer';
 import { GetProjectDauResponseDto } from './dto/get-project-dau-response.dto';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Project } from '../../project/entities/project.entity';
+import { DauMetric } from './metric/dau.metric';
 
 @Injectable()
 export class AnalyticsService {
@@ -15,22 +16,57 @@ export class AnalyticsService {
         private readonly analyticRepository: AnalyticsRepository,
     ) {}
 
-    async getProjectDAU(getProjectDAU: GetProjectDAU) {
-        const { projectName, date } = getProjectDAU;
+    async getDAUsByProject(getDAUByProjectDto: GetDAUsByProjectDto) {
+        const { projectName } = getDAUByProjectDto;
         const project = await this.projectRepository.findOne({
             where: { name: projectName },
             select: ['domain'],
         });
+        if (!project) throw new NotFoundException(`Project with name ${projectName} not found`);
 
-        if (!project) {
-            throw new NotFoundException(`Project with name ${projectName} not found`);
-        }
-        const dau = await this.analyticRepository.findDAUByProject(project.domain, date);
+        const { start, end } = this.calculateDAUsStartEndDate();
+        const dauRecords = await this.analyticRepository.findDAUsByProject(
+            project.domain,
+            start,
+            end,
+        );
+
+        const filledRecords = this.fillDauRecords(start, dauRecords);
 
         return plainToInstance(GetProjectDauResponseDto, {
             projectName,
-            date,
-            dau,
+            dauRecords: filledRecords,
         });
+    }
+
+    private calculateDAUsStartEndDate() {
+        const end = new Date();
+        const start = new Date(end);
+        start.setDate(end.getDate() - 29);
+        return { start, end };
+    }
+
+    private fillDauRecords(start: Date, originalRecords: DauMetric[]) {
+        const recordMap = new Map<string, number>();
+        originalRecords.forEach((record) => {
+            const dateKey = this.formatDate(record.date);
+            recordMap.set(dateKey, record.dau);
+        });
+
+        return Array.from({ length: 30 }, (_, i) => {
+            const date = new Date(start.getTime());
+            date.setDate(start.getDate() + i);
+            const dateKey = this.formatDate(date);
+            const dau = recordMap.get(dateKey) || 0;
+            return { date: dateKey, dau };
+        });
+    }
+
+    private formatDate(dateInput: Date): string {
+        const date = new Date(dateInput);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
     }
 }
