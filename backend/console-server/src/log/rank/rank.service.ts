4288ed43dc4625a442bf3f695e7d83d5a1363d51
sjy2335 <joey110951@gmail.com>
2024-11-26 21:50:19 +0900

    feat: 응답 소요시간 랭킹 service 메소드

--- backend/console-server/src/log/rank/rank.service.ts
+++ backend/console-server/src/log/rank/rank.service.ts
@@ -9,6 +9,11 @@ import {
     GetSuccessRateRankResponseDto,
     SuccessRateRank,
 } from './dto/get-success-rate-rank-response.dto';
+import { GetElapsedTimeRankDto } from './dto/get-elapsed-time-rank.dto';
+import {
+    ElapsedTimeRank,
+    GetElapsedTimeRankResponseDto,
+} from './dto/get-elapsed-time-rank-response.dto';
 
 @Injectable()
 export class RankService {
@@ -40,5 +45,47 @@ export class RankService {
         });
 
         return plainToInstance(GetSuccessRateRankResponseDto, { total: results.length, rank });
+    }
+
+    async getElapsedTimeRank(_getElapsedTimeRankDto: GetElapsedTimeRankDto) {
+        const results = await this.rankRepository.findHostOrderByElapsedTimeSince(
+            this.getYesterdayDateString(),
+        );
+
+        const hosts = results.map((result) => result.host);
+        const projectMap = await this.hostsToProjectMap(hosts);
+        const rank = results.map((result): ElapsedTimeRank => {
+            return {
+                projectName: projectMap.get(result.host) || `Unknown`,
+                elapsedTime: result.avg_elapsed_time,
+            };
+        });
+
+        return plainToInstance(GetElapsedTimeRankResponseDto, { total: rank.length, rank });
+    }
+
+    private getYesterdayDateString() {
+        const now = new Date();
+        const yesterday = new Date(now);
+        yesterday.setDate(now.getDate() - 1);
+        return this.formatDate(yesterday);
+    }
+
+    private formatDate(dateInput: Date) {
+        const date = new Date(dateInput);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    }
+
+    private async hostsToProjectMap(hosts: string[]) {
+        const projects = await this.projectRepository.find({
+            select: ['domain', 'name'],
+            where: {
+                domain: In(hosts),
+            },
+        });
+        return new Map(projects.map(({ domain, name }) => [domain, name]));
     }
 }
