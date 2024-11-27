0514c8f81aff2d5efc7355ef953ee4e81bc5b14b
Hosung99 <shson1217@kw.ac.kr>
2024-11-27 18:27:12 +0900

    fix: 배포관련 에러코드 수정

--- frontend/src/component/organism/ProjectDAU.tsx
+++ frontend/src/component/organism/ProjectDAU.tsx
@@ -7,13 +7,12 @@ type Props = {
 
 export default function ProjectDAU({ id }: Props) {
   // const { data } = useProjectDAU(id);
-
   return (
     <DataLayout cssOption='flex flex-col p-8 rounded-lg shadow-md w-full bg-white'>
       <div className='mb-8 text-center'>
         <h2 className='text-navy text-2xl font-bold'>DAU</h2>
       </div>
-      <div className='h-[220px] w-full'>요기 라이브러리 코드적용</div>
+      <div className='h-[220px] w-full'>요기 라이브러리 코드적용 ${id}</div>
     </DataLayout>
   );
 }
