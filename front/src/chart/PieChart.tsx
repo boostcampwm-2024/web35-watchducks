6a70a1da01342b5b23f5d76f7366ba3f15339a74
Hosung99 <shson1217@kw.ac.kr>
2024-11-27 17:04:16 +0900

    feat: PieChart 컴포넌트 추가

---
+++ front/src/chart/PieChart.tsx
@@ -0,0 +1,25 @@
+import { CHART_COLORS } from '@constant/Chart';
+import { merge } from 'lodash-es';
+
+import { Chart } from './Chart';
+
+type Props = {
+  series: number[];
+  options: ApexCharts.ApexOptions;
+};
+
+export default function PieChart({ series, options: additionalOptions }: Props) {
+  const donutChartOption: ApexCharts.ApexOptions = {
+    chart: {
+      height: 350,
+      type: 'donut'
+    },
+    legend: {
+      position: 'bottom'
+    },
+    colors: CHART_COLORS
+  };
+
+  const options = merge({}, donutChartOption, additionalOptions);
+  return <Chart type='donut' series={series} options={options} />;
+}
