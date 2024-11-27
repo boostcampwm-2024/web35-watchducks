d834635e18cbc171ab810256e3d8ebbf038f570b
sjy2335 <joey110951@gmail.com>
2024-11-28 00:59:29 +0900

    chore: 파일명 변경

--- backend/console-server/src/common/cache/cache-wrap.decorator.ts
+++
@@ -1,8 +0,0 @@
-import type { ExecutionContext } from '@nestjs/common';
-import { SetMetadata } from '@nestjs/common';
-
-export const CACHE_REFRESH_THRESHOLD_METADATA = 'CACHE_REFRESH_THRESHOLD';
-
-type CacheRefreshThresholdFactory = (ctx: ExecutionContext) => Promise<number> | number;
-export const CacheRefreshThreshold = (threshold: number | CacheRefreshThresholdFactory) =>
-    SetMetadata(CACHE_REFRESH_THRESHOLD_METADATA, threshold);
