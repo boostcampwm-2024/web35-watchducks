65457ce4cd22282576db97e4826b84f0c2a36d96
Hosung99 <shson1217@kw.ac.kr>
2024-11-28 22:36:29 +0900

    feat: 랭킹페이지 완성

--- frontend/src/component/page/RankingPage.tsx
+++ frontend/src/component/page/RankingPage.tsx
@@ -1,3 +1,19 @@
+import CustomErrorBoundary from '@boundary/CustomErrorBoundary';
+import RankingList from '@component/organism/RankingList';
+import { useNavContext } from '@hook/useNavContext';
+
+type Props = {
+  generation: string;
+};
+
 export default function RankingPage() {
-  return <div className='flex h-screen w-full bg-gray-50'>랭킹페이지입니다</div>;
+  const { generation } = useNavContext<Props>();
+
+  return (
+    <div className='flex h-screen w-full flex-col gap-8 bg-lightgray p-16 dark:bg-lightblack'>
+      <CustomErrorBoundary>
+        <RankingList generation={generation} />
+      </CustomErrorBoundary>
+    </div>
+  );
 }
