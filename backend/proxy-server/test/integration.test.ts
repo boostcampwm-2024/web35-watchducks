f2fc8609a834367b832d0fbc6d0c0a1f9debb14b
EnvyW6567 <88361948+EnvyW6567@users.noreply.github.com>
2024-12-02 15:23:03 +0900

    test: 통합테스트 구현
    - 의존성 주입 컨테이너를 이용한 데이터베이스 상호작용 객체를 모의객체로 변경
    - supertest 모듈을 이용한 실제 서버 동작 확인

--- backend/proxy-server/test/integration.test.ts
+++ backend/proxy-server/test/integration.test.ts
@@ -1,87 +1,116 @@
-import request from 'supertest';
-import { Server } from '../src/server/server';
-import { LogService } from '../src/domain/log/log.service';
-import { LogRepositoryClickhouse } from '../src/database/query/log.repository.clickhouse';
-import { ErrorLogRepository } from '../src/common/logger/error-log.repository';
-import type { ProjectService } from '../src/domain/project/project.service';
-import { ProxyService } from '../src/domain/proxy/proxy.service';
-import { fastifyConfig } from '../src/server/config/fastify.config';
-import * as dotenv from 'dotenv';
+import 'reflect-metadata';
+import type { FastifyInstance } from 'fastify';
+import supertest from 'supertest';
+import { fastifyServer } from '../src/server/fastify.server';
+import dotenv from 'dotenv';
+import type { Logger } from '../src/common/logger/createFastifyLogger';
+import type { ProjectRepository } from '../src/domain/port/output/project.repository';
+import type { LogRepository } from '../src/domain/port/output/log.repository';
+import type { ProjectCacheRepository } from '../src/domain/port/output/project-cache.repository';
+import { container } from 'tsyringe';
+import { TOKENS } from '../src/common/container/container';
+
+jest.mock('database/query/log.repository.clickhouse');
+jest.mock('database/query/project.repository.mysql');
+jest.mock('database/query/project-cache.repository.redis');
 
 dotenv.config({ path: '../.env' });
 
-describe('Proxy Server 통합 테스트', () => {
-    let server: Server;
-    const targetAddress = 'https://' + process.env.TEST_TARGET_ADDRESS;
-    console.log(targetAddress);
+describe('프록시 서버 통합 테스트', () => {
     const proxyPort = 3000;
+    const targetAddress = 'http://localhost:3100';
+
+    let server: FastifyInstance;
+    let logger: Logger;
+
+    let mockProjectRepository: jest.Mocked<ProjectRepository>;
+    let mockLogRepository: jest.Mocked<LogRepository>;
+    let mockProjectCacheRepository: jest.Mocked<ProjectCacheRepository>;
+
+    process.env.PORT = proxyPort.toString();
+    process.env.LISTENING_HOST = 'localhost';
+    process.env.DEFAULT_CONNECTIONS = '10';
+    process.env.DEFAULT_PIPELINING = '5';
+    process.env.DEFAULT_KEEP_ALIVE = '5000';
 
     beforeAll(async () => {
-        process.env.PORT = proxyPort.toString();
-        process.env.LISTENING_HOST = 'localhost';
-        process.env.DEFAULT_CONNECTIONS = '10';
-        process.env.DEFAULT_PIPELINING = '5';
-        process.env.DEFAULT_KEEP_ALIVE = '5000';
+        mockProjectRepository = {
+            findIpByDomain: jest.fn(),
+        } as unknown as jest.Mocked<ProjectRepository>;
+        mockLogRepository = {
+            insertHttpLog: jest.fn(),
+        } as unknown as jest.Mocked<LogRepository>;
+        mockProjectCacheRepository = {
+            findIpByDomain: jest.fn(),
+            cacheIpByDomain: jest.fn(),
+        } as unknown as jest.Mocked<ProjectCacheRepository>;
 
-        Object.assign(fastifyConfig, {
-            bodyLimit: 1048576,
-            requestTimeout: 30000,
-            keepAliveTimeout: 5000,
-        });
+        container.registerInstance(TOKENS.PROJECT_REPOSITORY, mockProjectRepository);
+        container.registerInstance(TOKENS.LOG_REPOSITORY, mockLogRepository);
+        container.registerInstance(TOKENS.PROJECT_CACHE_REPOSITORY, mockProjectCacheRepository);
 
-        class MockLogRepository extends LogRepositoryClickhouse {
-            constructor() {
-                super();
-            }
-
-            async insertHttpLog() {
-                return Promise.resolve();
-            }
-        }
-
-        const mockLogRepository = new MockLogRepository();
-
-        const mockProjectService: ProjectService = {
-            getIpByDomain: jest.fn().mockImplementation(async (_domain: string) => {
-                return process.env.TEST_TARGET_ADDRESS;
-            }),
-        } as unknown as ProjectService;
-
-        const logService = new LogService(mockLogRepository);
-        const errorLogRepository = new ErrorLogRepository();
-        const proxyService = new ProxyService(mockProjectService);
-
-        try {
-            server = new Server(
-                errorLogRepository,
-                proxyService,
-                logService
-            );
-
-            await server.start();
-        } catch (error) {
-            console.error('Server initialization failed:', error);
-            throw error;
-        }
+        const { server: serverInstance, logger: loggerInstance } = await fastifyServer.listen();
+        server = serverInstance;
+        logger = loggerInstance;
     });
 
     afterAll(async () => {
-        if (server) {
-            await server.stop();
-        }
+        fastifyServer.stop(server, logger);
     });
 
-    describe('Proxy Server 테스트', () => {
-        it('프록시 서버가 요청을 올바르게 처리할 수 있어야 한다.', async () => {
-            const response = await request(targetAddress)
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    describe('프록시 서버는', () => {
+        it('올바른 요청에 대해 200을 반환하고 로그 삽입 로직을 실행시켜야한다.', async () => {
+            mockProjectRepository.findIpByDomain.mockResolvedValue('175.106.99.193');
+            mockLogRepository.insertHttpLog.mockResolvedValue();
+            mockProjectCacheRepository.findIpByDomain.mockResolvedValue(null);
+
+            const response = await supertest(targetAddress)
                 .get('/api')
                 .set('Host', 'watchducks-test.store')
                 .set('Accept', '*/*')
                 .set('Accept-Encoding', 'gzip, deflate, br')
                 .set('Connection', 'keep-alive');
 
-            console.log('Response Status:', response.status);
+            console.log('Response Status:', response.body);
             expect([200, 502]).toContain(response.status);
+            expect(mockLogRepository.insertHttpLog).toHaveBeenCalled();
+            expect(mockLogRepository.insertHttpLog).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    method: 'GET',
+                    path: '/api',
+                    host: 'watchducks-test.store',
+                    statusCode: expect.any(Number),
+                }),
+            );
+        });
+
+        it('등록되지 않은 도메인에 대한 요청에 대해서 404를 반환해야하고 로그 삽입 로직을 실행시켜야한다.', async () => {
+            mockProjectRepository.findIpByDomain.mockResolvedValue('');
+            mockLogRepository.insertHttpLog.mockResolvedValue();
+            mockProjectCacheRepository.findIpByDomain.mockResolvedValue(null);
+
+            const response = await supertest(targetAddress)
+                .get('/api')
+                .set('Host', 'watchducks-test.store')
+                .set('Accept', '*/*')
+                .set('Accept-Encoding', 'gzip, deflate, br')
+                .set('Connection', 'keep-alive');
+
+            console.log('Response Status:', response.body);
+            expect(404).toBe(response.status);
+            expect(mockLogRepository.insertHttpLog).toHaveBeenCalled();
+            expect(mockLogRepository.insertHttpLog).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    method: 'GET',
+                    path: '/api',
+                    host: 'watchducks-test.store',
+                    statusCode: expect.any(Number),
+                }),
+            );
         });
     });
 });
