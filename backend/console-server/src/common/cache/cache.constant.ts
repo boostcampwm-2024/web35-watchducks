a64df08fdae45ab344daff1ebb8b103f943c7e28
EnvyW6567 <88361948+EnvyW6567@users.noreply.github.com>
2024-12-02 16:46:12 +0900

    ??

---
+++ backend/console-server/src/common/cache/cache.constant.ts
@@ -0,0 +1,11 @@
+export const CACHE_REFRESH_THRESHOLD_METADATA = 'CACHE_REFRESH_THRESHOLD';
+
+export const FIFTEEN_SECONDS = 15 * 1000;
+export const THIRTY_SECONDS = 30 * 1000;
+export const ONE_MINUTE = 60 * 1000;
+export const ONE_MINUTE_HALF = 3 * 30 * 1000;
+export const THREE_MINUTES = 3 * 60 * 1000;
+export const FIVE_MINUTES = 5 * 60 * 1000;
+export const TEN_MINUTES = 10 * 60 * 1000;
+export const HALF_HOUR = 30 * 60 * 1000;
+export const ONE_HOUR = 60 * 60 * 1000;
