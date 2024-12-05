1a279cad8030653304446ee8ccf49ee7a233c4b1
Hosung99 <shson1217@kw.ac.kr>
2024-12-05 14:44:44 +0900

    refactor: ErrorFallback 스켈레톤 UI수정

--- frontend/src/boundary/CustomErrorFallback.tsx
+++ frontend/src/boundary/CustomErrorFallback.tsx
@@ -1,4 +1,3 @@
-import Loading from '@component/atom/Loading';
 import { useQueryErrorResetBoundary } from '@tanstack/react-query';
 
 type Props = {
@@ -15,7 +14,7 @@ export default function CustomErrorFallback({ resetErrorBoundary }: Props) {
 
   return (
     <div className='flex flex-col items-center justify-center gap-4'>
-      <Loading />
+      <h1 className='text-black dark:text-white'>요청이 만료됐습니다. 재시도해주세요</h1>
       <button
         onClick={handleClickReset}
         className='flex items-center gap-2 rounded-lg bg-blue px-6 py-2 text-white shadow-md transition-all duration-200 hover:bg-opacity-90 hover:text-black active:scale-95'>
